6a0b47039c9696a5c5be71a12b46b089773197d6
Manu199 <manu199gh@gmail.com>
2023-09-11 14:52:51 +0200

    [add] java if and box colors

--- js/script.js
+++ js/script.js
@@ -6,6 +6,17 @@ for(let i = 0; i < 100; i++){
 
   if(!((i + 1) % 3)){
     box.classList.add('bg-green');
+    box.textContent = 'Fizz';
+  }
+
+  if(!((i + 1) % 5)){
+    box.classList.add('bg-yellow');
+    box.textContent = 'Buzz';
+  }
+
+  if (!((i + 1) % 3) && !((i + 1) % 5)) {
+    box.classList.add('bg-pink');
+    box.textContent = 'FizzBuzz';
   }
 
   boxContainer.append(box);
