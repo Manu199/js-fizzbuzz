2e22ebb7802faa512443f22ee80067c1f88f5b3b
Manu199 <manu199gh@gmail.com>
2023-09-11 17:42:54 +0200

    [update] readme

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+
+// - generare 100 box numerati
+// - colorare tutti i box multipli di 3 di verde e cambiare il contenuto con la scritta "Fizz"
+// - colorare tutti i box multipli di 5 di giallo e cambiare il contenuto con la scritta "Buzz"
+// - colorare tutti i box multipli dia di 3 che di 5 di rosa e cambiare il contenuto con la scritta "FizzBuzz"
+
+
+
+
 const boxContainer = document.querySelector('.boxes');
 for(let i = 1; i < 100; i++){
   const box = document.createElement('div');
