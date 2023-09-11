c0ab1e3c63edfc500183bb87ab9344f6d5c91076
Manu199 <manu199gh@gmail.com>
2023-09-11 14:17:19 +0200

    [add] boxes

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const boxContainer = document.querySelector('.boxes');
+for(let i = 0; i < 100; i++){
+  const box = document.createElement('div');
+  box.classList.add('box');
+  box.append(i + 1);
+
+  if(!((i + 1) % 3)){
+    box.classList.add('bg-green');
+  }
+
+  boxContainer.append(box);
+}
